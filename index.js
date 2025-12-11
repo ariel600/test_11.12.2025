f57f7742d9bb1dee76d60d1e93621373e8c4436e
Ariel_Naiman <a0527633600@gmail.com>
2025-12-11 09:41:09 +0200

    finish index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+import { menu } from "./utils/prints.js";
+import { buyingShares, showingStock, stockSearch } from "./utils/utils.js";
+
+function main(){
+    let start = true
+    do{
+        switch(menu()){
+            case 1: stockSearch(); break;
+            case 2: showingStock(); break;
+            case 3: buyingShares(); break;
+            case 4: start = false;
+            default: console.log("Wrong choice.")
+        }
+    } while(start);
+}
+main
