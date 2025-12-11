ab80ebb8a862c4c6f67fb12e47cdf5d5eebbe379
Ariel_Naiman <a0527633600@gmail.com>
2025-12-11 12:45:00 +0200

    finish test

--- index.js
+++ index.js
@@ -2,15 +2,14 @@ import { menu } from "./utils/prints.js";
 import { buyingShares, showingStock, stockSearch } from "./utils/utils.js";
 
 function main(){
-    let start = true
     do{
         switch(menu()){
             case 1: stockSearch(); break;
             case 2: showingStock(); break;
             case 3: buyingShares(); break;
-            case 4: start = false; break;
+            case 4: return
             default: console.log("Wrong choice.")
         }
-    } while(start);
+    } while(true);
 }
 main()
